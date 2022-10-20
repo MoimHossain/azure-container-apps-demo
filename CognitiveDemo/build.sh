276d1a5c0d82f748f19c2da28f3779b70ee2980a
Moim HA <moimhossain@gmail.com>
2022-10-20 10:38:04 +0200

    Fixing the path

--- CognitiveDemo/build.sh
+++ CognitiveDemo/build.sh
@@ -5,7 +5,7 @@ RegistryName=$2
 
 echo "Building Images with Tag $ImageTag"
 
-docker build -t $RegistryName/job-listener:$ImageTag -f ./JobListener/Dockerfile ./JobListener  
+docker build -t $RegistryName/job-listener:$ImageTag -f ${PWD}/CognitiveDemo/JobListener/Dockerfile ${PWD}/CognitiveDemo/JobListener  
 
 
 
