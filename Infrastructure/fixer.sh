496d0863667a91ef63619184f5244230a6dd2de8
Moim HA <moimhossain@gmail.com>
2022-10-24 08:26:49 +0200

    Updated az cli extension for capp

--- Infrastructure/fixer.sh
+++ Infrastructure/fixer.sh
@@ -3,6 +3,10 @@ COMMITHASH=$1
 
 echo "Starting script...Commit Hash received $COMMITHASH"
 
+
+az config set extension.use_dynamic_install=yes_without_prompt
+az extension add -n containerapp
+
 nextRevisionName="${COMMITHASH:0:10}"
 previousRevisionName=$(az containerapp revision list -n xeniel-frontend -g xeniel --query '[0].name')
 
