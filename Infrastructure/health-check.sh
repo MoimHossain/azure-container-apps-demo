309e4f765572206e5e1e4dde407531b874d8336e
Moim HA <moimhossain@gmail.com>
2022-10-27 07:10:14 +0200

    Incorrect name in workflow

--- Infrastructure/health-check.sh
+++ Infrastructure/health-check.sh
@@ -3,6 +3,8 @@ COMMITHASH=$1
 
 nextRevisionName="xeniel-frontend--${COMMITHASH:0:10}"
 
+echo "Invoking https://$nextRevisionName.jollytree-a241632e.westeurope.azurecontainerapps.io/health"
+
 status_code=$(curl --write-out %{http_code} --silent --output /dev/null "https://$nextRevisionName.jollytree-a241632e.westeurope.azurecontainerapps.io/health")
 
 echo "status_code: $status_code"
